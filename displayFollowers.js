7142934853145052ed9440d252cfb6e22953de48
21cloudocean <437508630@qq.com>
2022-07-19 10:25:03 +0800

    displayFollowers

--- displayFollowers.js
+++ displayFollowers.js
@@ -1,3 +1,17 @@
-const display = () => {}
+const container = document.querySelector(".container");
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      const { avatar_url: img, login: name, html_url: pageURL } = person;
+      return `<article class="card">
+    <img src="${img}" alt="${name}"/>
+    <h4>${name}</h4>
+    <a href="${pageURL}" class="btn">view profile</a>
+    </article>`;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
+};
+
+export default display;
